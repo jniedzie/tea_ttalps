8f8ed6926eb4bf4c50adf6e69bf24531570e855f
Lovisa <lovisa.rygaard@cern.ch>
2025-01-28 12:31:48 +0100

    LLP triggers skim config

---
+++ configs/ttalps_skimmer_looseSemimuonic_LLPtrigger.py
@@ -0,0 +1,48 @@
+from ttalps_extra_collections import extraEventCollections
+from golden_json_config import goldenJson
+
+nEvents = -1
+printEveryNevents = 10000
+
+applyLooseSkimming = True
+applyTTbarLikeSkimming = False
+applyTTZLikeSkimming = False
+applySignalLikeSkimming = False
+
+# weightsBranchName = ""
+weightsBranchName = "genWeight"
+eventsTreeNames = ("Events",)
+
+triggerSelection = (
+    "HLT_DoubleL2Mu23NoVtx_2Cha",
+    "HLT_DoubleL2Mu23NoVtx_2Cha_CosmicSeed",
+)
+
+eventCuts = {
+    "MET_pt": (30, 9999999),
+    "nLoosePATMuons": (1, 9999999),
+    "nGoodJets": (4, 9999999),
+    "nGoodMediumBtaggedJets": (1, 9999999),
+}
+
+requiredFlags = (
+    "Flag_goodVertices",
+    "Flag_globalSuperTightHalo2016Filter",
+    "Flag_HBHENoiseFilter",
+    "Flag_HBHENoiseIsoFilter",
+    "Flag_EcalDeadCellTriggerPrimitiveFilter",
+    "Flag_BadPFMuonFilter",
+    "Flag_BadPFMuonDzFilter",
+    "Flag_hfNoisyHitsFilter",
+    "Flag_eeBadScFilter",
+    "Flag_ecalBadCalibFilter",
+)
+
+branchesToKeep = ["*"]
+branchesToRemove = []
+specialBranchSizes = {
+  "Proton_multiRP": "nProton_multiRP",
+  "Proton_singleRP": "nProton_singleRP",
+}
+
+redirector = "xrootd-cms.infn.it"
