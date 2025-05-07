9c60fab7edbb37fd83d0c1285638d9b9965fccf5
Jeremi Niedziela <jeremi.niedziela@cern.ch>
2025-05-07 09:33:25 +0200

    Remiving muon matching params from the e+jets ttbar CR skimmer config.

--- configs/ttalps_skimmer_ttbarLike_semielectronic_config.py
+++ configs/ttalps_skimmer_ttbarLike_semielectronic_config.py
@@ -13,21 +13,6 @@ applyTTZLikeSkimming = False
 weightsBranchName = "genWeight"
 eventsTreeNames = ("Events",)
 
-# For the signal like skimming all given muonMatchingParams are applied together
-# If only one matching method should be used ONLY include that one method
-# We might want to update the logic of this in the future
-# Matching methods implemented are:
-# "Segment" : max matching ratio (eg. 2.0/3.0)
-# "DR" : max Delta R (eg. 0.1)
-# "OuterDR" : max Delta R (eg. 0.1)
-# "ProxDR" : max Delta R (eg. 0.1)
-muonMatchingParams = {
-    "Segment": 2.0/3.0,
-    # "DR" : 0.1,
-    # "OuterDR" : 0.1,
-    # "ProxDR" : 0.1,
-}
-
 eventCuts = {
     "MET_pt": (50, 9999999),
     "nLooseElectrons": (1, 1),
