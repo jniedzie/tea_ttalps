d7b920cbf2b238fc3db161bfa177a74baa28cb55
Jeremi Niedziela <jeremi.niedziela@cern.ch>
2025-07-24 09:49:25 +0200

    Fixing the issue with non-unique DAS file names.

--- configs/ttalps_skimmer_das_files_config.py
+++ configs/ttalps_skimmer_das_files_config.py
@@ -1,7 +1,8 @@
-from ttalps_samples_list import dasBackgrounds2016
-from ttalps_samples_list import dasSamples2018, dasData2018, dasSignals2018, dasBackgrounds2018, dasData2018_standard
-from ttalps_samples_list import dasBackgrounds2022preEE
+from ttalps_samples_list import dasBackgrounds2016PreVFP, dasData2016PreVFP
+from ttalps_samples_list import dasBackgrounds2016PostVFP, dasData2016PostVFP
 import os
+import subprocess
+from Logger import info
 
 max_files = -1
 
@@ -18,7 +19,7 @@ output_skim = "skimmed_looseSemimuonic_v2"
 # output_skim = "skimmed_looseSemimuonicv1_LLPtrigger_unmerged"
 
 # Loose non-ttbar skims, vetoing events with too many (b-)jets
-# output_skim = "skimmed_looseNonTT_v1"  
+# output_skim = "skimmed_looseNonTT_v1"
 # output_skim = "skimmed_looseNoBjets_lt4jets_v1"
 # output_skim = "skimmed_loose_lt3bjets_lt4jets_v1"
 
@@ -28,28 +29,34 @@ output_skim = "skimmed_looseSemimuonic_v2"
 # output_skim = "skimmed_looseInvertedMet_v1"
 # output_skim = "skimmed_looseNoMet_v1"
 
-output_trees_dir = ""
-output_hists_dir = ""
-input_directory = ""
-
 dbs_instance = "prod/phys03"
 # dbs_instance = "prod/global"
 
-# dasSamples = dasBackgrounds2016
+dasSamples = dasData2016PostVFP
 
-dasSamples = dasSamples2018
+# dasSamples = dasSamples2022postEE
 # dasSamples = dasData2018
 # dasSamples = dasSignals2018
 # dasSamples = dasBackgrounds2018
 # dasSamples = dasData2018_standard
-
 # dasSamples = dasBackgrounds2022preEE
 
-dirs = [(v, f"{base_path}/{k}/{output_skim}/") for k, v in dasSamples.items()]
-
 # # For DAS datasets:
-dataset = ""
-datasets_and_output_trees_dirs = dirs
+input_output_file_list = []
+
+for path, das_dataset in dasSamples.items():
+  command = f"dasgoclient --query='file dataset={das_dataset} instance={dbs_instance}'"
+  info(f"Running command: {command}")
+  das_files = subprocess.check_output(command, shell=True, text=True).strip().split('\n')
+
+  for input_path in das_files:
+
+    input_file_name = input_path.split('/')[-1].split('.')[0]
+    input_part = input_path.split('/')[-2]
+    input_date_tag = input_path.split('/')[-3]
+
+    input_output_file_list.append((input_path, f"{base_path}/{path}/{output_skim}/{input_file_name}_{input_date_tag}_{input_part}.root", "/dev/null"))
+
 
 # # For local path to DAS files:
 # input_dasfiles = ""
