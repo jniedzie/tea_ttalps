40242215753180efa0bcdfbe4064b8eb07d6c6a4
Jeremi Niedziela <jeremi.niedziela@cern.ch>
2026-02-16 11:09:27 +0100

    Adjusting the apps for the new version of tea.

--- apps/ttalps_histogrammer.cpp
+++ apps/ttalps_histogrammer.cpp
@@ -12,27 +12,10 @@
 using namespace std;
 
 int main(int argc, char **argv) {
-  auto args = make_unique<ArgsManager>(argc, argv);
-  if (!args->GetString("config").has_value()) {
-    fatal() << "No config file provided" << endl;
-    exit(1);
-  }
-
-  ConfigManager::Initialize(args->GetString("config").value());
-  auto &config = ConfigManager::GetInstance();
-
-  if (args->GetString("input_path").has_value()) {
-    config.SetInputPath(args->GetString("input_path").value());
-  }
-  if (args->GetString("output_path").has_value()) {
-    config.SetHistogramsOutputPath(args->GetString("output_path").value());
-  }
-  if (args->GetString("output_hists_path").has_value()) {
-    config.SetHistogramsOutputPath(args->GetString("output_hists_path").value());
-  }
-  if (args->GetString("output_trees_path").has_value()) {
-    config.SetTreesOutputPath(args->GetString("output_trees_path").value());
-  }
+  vector<string> requiredArgs = {"config"};
+  vector<string> optionalArgs = {"input_path", "output_hists_path"};
+  auto args = make_unique<ArgsManager>(argc, argv, requiredArgs, optionalArgs);
+  ConfigManager::Initialize(args);
 
   info() << "Creating objects..." << endl;
   auto eventReader = make_shared<EventReader>();
@@ -49,6 +32,9 @@ int main(int argc, char **argv) {
   bool runABCDHistograms, runSingleMuonABCDHistograms, runABCDMothersHistograms, runFakesHistograms;
   bool ignoreDimuons, runNminus1Histograms;
   bool runGenLevelResonancesABCD, runGenLevelMothersABCD, runRevertedMatching, noWeights;
+
+  auto &config = ConfigManager::GetInstance();
+
   config.GetValue("runDefaultHistograms", runDefaultHistograms);
   config.GetValue("runLLPTriggerHistograms", runLLPTriggerHistograms);
   config.GetValue("runLLPNanoAODHistograms", runLLPNanoAODHistograms);
