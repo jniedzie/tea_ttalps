196499f0e4bdc30f210a8141aa3f2e239fa1c210
annaprzybyl <andziaprzyb@gmail.com>
2026-07-01 11:04:29 +0200

    Add semielectronic skimmer config

---
+++ configs/ttalps_skimmer_signalLike_semielectronic_config.py
@@ -0,0 +1,31 @@
+from ttalps_extra_collections import get_extra_event_collections
+from ttalps_object_cuts import *
+from scale_factors_config import get_scale_factors
+from ttalps_skimmer_files_config import year
+
+extraEventCollections = get_extra_event_collections(year)
+scaleFactors = get_scale_factors(year)
+
+nEvents = -1
+
+applyLooseSkimming = False
+applyTTZLikeSkimming = False
+
+weightsBranchName = "genWeight"
+eventsTreeNames = ("Events",)
+
+eventCuts = {
+    "MET_pt": (50, 9999999),
+    "nTightElectrons": (1, 9999999),
+    "nLooseMuons": (2, 9999999),
+    # "nLooseElectrons": (0, 0),
+    # "nGoodTightBtaggedJets": (2, 9999999),  # TODO: consider tight WP and/or 2 b-tags
+}
+
+branchesToKeep = ["*"]
+branchesToRemove = []
+
+specialBranchSizes = {
+    "Proton_multiRP": "nProton_multiRP",
+    "Proton_singleRP": "nProton_singleRP",
+}
